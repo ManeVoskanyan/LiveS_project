3386532e33300f59a0b5bb4db496cbff069d43ec
ManeVoskanyan <manevoskanyan7mm@gmail.com>
2024-03-04 22:27:57 +0400

    Initial search on google map

--- app/src/main/java/com/example/lives_project/DogsLessonsStartFragment.java
+++ app/src/main/java/com/example/lives_project/DogsLessonsStartFragment.java
@@ -7,12 +7,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DogsLessonsStartFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_dogs_lessons_start, container, false);
+   View view = inflater.inflate(R.layout.fragment_dogs_lessons_start, container, false);
+//        TextView start1 = view.findViewById(R.id.dogs_start_lesson1);
+//        TextView start2 = view.findViewById(R.id.dogs_start_lesson2);
+//        ImageView imageView = view.findViewById(R.id.imageView);
+//        start1.animate().alpha(0);
+//        start2.animate().alpha(0);
+//        imageView.animate().alpha(0);
+//        start1.animate().alpha(1);
+//        start2.animate().alpha(1);
+//        imageView.animate().alpha(1);
+        return view;
     }
 }
--- app/src/main/java/com/example/lives_project/FullscreenMapActivity.java
+++ app/src/main/java/com/example/lives_project/FullscreenMapActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -18,68 +19,100 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.SearchView;
 import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.List;
 
-public class FullscreenMapActivity  extends AppCompatActivity implements OnMapReadyCallback {
+public class FullscreenMapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-                    private GoogleMap myMap;
-                    private SearchView mapSearchView;
+    private GoogleMap myMap;
+    private EditText mapSearchView;
 
-                    protected void onCreate(Bundle savedInstanceState) {
-                        super.onCreate(savedInstanceState);
-                        try {
-                        setContentView(R.layout.fullscreen_map_activity);
-                        mapSearchView = findViewById(R.id.mapSearch);
-                        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_google);
-                        mapSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-                            @Override
-                            public boolean onQueryTextSubmit(String query) {
-                                String location = mapSearchView.getQuery().toString();
-                                List<Address> addressList = null;
-                                if (location != null) {
-                                    Geocoder geocoder = new Geocoder(FullscreenMapActivity.this);
-                                    try {
-                                        addressList = geocoder.getFromLocationName(location, 1);
-                                    } catch (IOException e) {
-                                        e.printStackTrace();
-                                    }
-                                    Address address = addressList.get(0);
-                                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
-                                    MarkerOptions options = new MarkerOptions().position(latLng).title(location);
-                                    options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
-                                    myMap.addMarker(options);
-                                    myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
-                                }
-                                return false;
-                            }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            setContentView(R.layout.fullscreen_map_activity);
+            mapSearchView = findViewById(R.id.mapSearch);
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_google);
+            mapSearchView.addTextChangedListener(new TextWatcher() {
+                                                     @Override
+                                                     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-                            @Override
-                            public boolean onQueryTextChange(String newText) {
-                                return false;
-                            }
-                        });
-                        mapFragment.getMapAsync(FullscreenMapActivity.this);
-                    }catch (Exception e){
-                            e.printStackTrace();
+                                                     }
 
-                        }
+                                                     @Override
+                                                     public void onTextChanged(CharSequence s, int start, int before, int count) {
 
-                    }
-            @Override
-            public void onMapReady(@NonNull GoogleMap googleMap) {
-             myMap = googleMap;
+                                                     }
 
-            }
+                                                     @Override
+                                                     public void afterTextChanged(Editable s) {
+                                                         String location = s.toString();
+                                                         List<Address> addressList = null;
+                                                         Geocoder geocoder = new Geocoder(FullscreenMapActivity.this);
+                                                         try {
+                                                             addressList = geocoder.getFromLocationName(location, 1);
+                                                             if (addressList.isEmpty()) {
+                                                                 return;
+                                                             }
+                                                             Address address = addressList.get(0);
+                                                             LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                                                             MarkerOptions options = new MarkerOptions().position(latLng).title(location);
+                                                             options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+                                                             myMap.addMarker(options);
+                                                             myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                                                         } catch (Exception e) {
+                                                             e.printStackTrace();
+                                                         }
+                                                     }
+                                                 }
+                    //{
+//                            @Override
+//                            public boolean onQueryTextSubmit(String query) {
+//                                String location = mapSearchView.getQuery().toString();
+//                                List<Address> addressList = null;
+//                                if (location != null) {
+//                                    Geocoder geocoder = new Geocoder(FullscreenMapActivity.this);
+//                                    try {
+//                                        addressList = geocoder.getFromLocationName(location, 1);
+//                                    } catch (IOException e) {
+//                                        e.printStackTrace();
+//                                    }
+//                                    Address address = addressList.get(0);
+//                                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+//                                    MarkerOptions options = new MarkerOptions().position(latLng).title(location);
+//                                    options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+//                                    myMap.addMarker(options);
+//                                    myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+//                                }
+//                                return false;
+//                            }
+//
+//                            @Override
+//                            public boolean onQueryTextChange(String newText) {
+//                                return false;
+//                            }
+//                        }
+            );
+            mapFragment.getMapAsync(FullscreenMapActivity.this);
+        } catch (Exception e) {
+            e.printStackTrace();
 
-    public void onLessonsClick(View v) {
-        Intent intent = new Intent(FullscreenMapActivity.this, LessonsActivity.class);
-        startActivity(intent);
+        }
+
     }
 
-                }
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        myMap = googleMap;
+
+    }
+
+}
 
